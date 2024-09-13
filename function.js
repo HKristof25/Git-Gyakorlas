9c2dfd3eafd92faefeb43b694a1f01cbfe4763a8
Hajdu Kristóf <hajdu.kristof@mechwart.local>
2024-09-13 09:02:55 +0200

    all elements

--- function.js
+++ function.js
@@ -1,10 +1,16 @@
 function getFirstElement (arr){
-        return arr[0]
+        return arr
     }
-    
+export function factorial(n) {
+            return Math.factorial(n)
+        }
+        
+    console.log(`Factorial of 5: ${factorial(5)}`);
+        
     const numbers = [1, 2, 3];
     const words = ["apple", "banana", "cherry"];
     
     console.log(`First number: ${getFirstElement(numbers)}`);
     console.log(`First word: ${getFirstElement(words)}`);
+
     
